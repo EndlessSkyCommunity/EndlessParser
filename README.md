8431140e6a8b0b4f5abef52b9add6bf645351e76
M*C*O <mcofficer@gmx.de>
2019-12-24 16:16:24 +0100

    Add Usage to README, improved setup.py, version bump

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # EndlessParser
 A robust Parser for Endless Sky's data files
 
+## Usage
+````bash
+$ pip install endlessparser
+````
+
+````python
+>>> from endlessparser import parse
+>>> with open("map.txt", "r") as f:
+...     nodes = parse(f.read())
+...
+>>> nodes[0]
+GalaxyNode(node_type='galaxy', tokens=['"Milky Way"'], children=[Node(node_type='pos', tokens=['0', '0'], children=[]), Node(node_type='sprite', tokens=['ui/galaxy'], children=[])])
+>>> nodes[0].name()
+'"Milky Way"'
+>>> nodes[0].sprite()
+'ui/galaxy'
+>>> nodes[0].position()
+(0.0, 0.0)
+````
+
 ## Contributing
 Requirements:
 - Python >= 3.7 for syntactic sugar
--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+import setuptools
 from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name="EndlessParser",
-    version="0.1",
+    version="0.2",
     description="A robust Parser for Endless Sky's data files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/EndlessSkyCommunity/EndlessParser",
     author="MCOfficer",
     author_email="mcofficer@gmx.de",
     license="GPL3",
-    packages=["endlessparser", "endlessparser.datatypes"],
+    packages=setuptools.find_packages(),
     zip_safe=False,
+    python_requires=">=3.7",
 )
