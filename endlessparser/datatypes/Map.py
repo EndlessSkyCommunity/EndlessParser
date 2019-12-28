9025cbb7a4144ff07d45cfdce3af1df0eaed552c
M*C*O <mcofficer@gmx.de>
2019-12-28 15:12:42 +0100

    add offset to ObjectNode

--- endlessparser/datatypes/Map.py
+++ endlessparser/datatypes/Map.py
@@ -22,6 +22,24 @@ class GalaxyNode(HasName, HasSprite, HasPosition):
 class SystemNode(HasName, HasPosition, HasObjects, HasMusic):
     node_type = "system"
 
+    def asteroids(self) -> Dict[str, Tuple[float, float]]:
+        d = {}
+        for child in self._find_children("asteroids"):
+            if len(child.tokens) >= 3:
+                d[child.tokens[0]] = (float(child.tokens[1]), float(child.tokens[2]))
+        return d
+
+    def belt(self) -> Optional[float]:
+        child = self._find_child("belt")
+        return float(child.tokens_as_string()) if child else None
+
+    def fleets(self) -> Dict[str, float]:
+        d = {}
+        for child in self._find_children("fleet"):
+            if len(child.tokens) >= 2:
+                d[child.tokens[0]] = float(child.tokens[1])
+        return d
+
     def government(self) -> Optional[str]:
         child = self._find_child("government")
         return child.tokens_as_string() if child else None
@@ -30,19 +48,12 @@ class SystemNode(HasName, HasPosition, HasObjects, HasMusic):
         child = self._find_child("habitable")
         return float(child.tokens_as_string()) if child else None
 
-    def belt(self) -> Optional[float]:
-        child = self._find_child("belt")
-        return float(child.tokens_as_string()) if child else None
+    def haze(self):
+        child = self._find_child("haze")
+        return child.tokens_as_string() if child else None
 
     def links(self) -> List[str]:
         return [child.tokens_as_string() for child in self._find_children("link")]
-
-    def asteroids(self) -> Dict[str, Tuple[float, float]]:
-        d = {}
-        for child in self._find_children("asteroids"):
-            if len(child.tokens) >= 3:
-                d[child.tokens[0]] = (float(child.tokens[1]), float(child.tokens[2]))
-        return d
 
     def minables(self) -> Dict[str, Tuple[float, float]]:
         d = {}
@@ -58,23 +69,16 @@ class SystemNode(HasName, HasPosition, HasObjects, HasMusic):
                 d[child.tokens[0]] = float(child.tokens[1])
         return d
 
-    def fleets(self) -> Dict[str, float]:
-        d = {}
-        for child in self._find_children("fleet"):
-            if len(child.tokens) >= 2:
-                d[child.tokens[0]] = float(child.tokens[1])
-        return d
-
-    def haze(self):
-        child = self._find_child("haze")
-        return child.tokens_as_string() if child else None
-
 
 class ObjectNode(HasName, HasSprite, HasObjects, Node):
     node_type = "object"
 
     def distance(self) -> float:
         child = self._find_child("distance")
+        return float(child.tokens_as_string()) if child else None
+
+    def offset(self) -> float:
+        child = self._find_child("offset")
         return float(child.tokens_as_string()) if child else None
 
     def period(self) -> float:
